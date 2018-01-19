44d1e53508151189129d814d491b1bae103bf5fe
Utilisateur invité <Guest@MacBook-Pro-de-Administrateur.local>
2018-01-19 08:22:44 +0100

    Added missing files

--- JSONList/PostListViewController.swift
+++ JSONList/PostListViewController.swift
@@ -6,4 +6,39 @@
 //  Copyright © 2018 Utilisateur invité. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class PostListViewController : UITableViewController {
+    
+    var jsonArray  = [[String: Any]]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let url = "https://jsonplaceholder.typicode.com/posts"
+        URLSession.shared.dataTask(with: URL(string:url)!) { data, response, error in
+            let jsonObject = try! JSONSerialization.jsonObject(with: data!, options: [])
+            self.jsonArray = jsonObject as! [[String: Any]]
+            
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+            
+        }.resume()
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return jsonArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell")!
+        
+        let postJSON = jsonArray[indexPath.row]
+        
+        cell.textLabel?.text = postJSON["title"] as? String
+        
+        return cell
+    }
+    
+}
