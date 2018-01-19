37db724d8c45ba6e5e1e9b440f11b03dbc869118
Utilisateur invité <Guest@MacBook-Pro-de-Administrateur.local>
2018-01-19 09:41:21 +0100

    Downloading an image and displaying it in a UIImageView

--- JSONList/PostDetailViewController.swift
+++ JSONList/PostDetailViewController.swift
@@ -10,11 +10,26 @@ import UIKit
 
 class PostDetailViewController : UIViewController {
     @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
     
     var postJSON : [String: Any]!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         titleLabel.text = postJSON["title"] as? String
+
+        loadImage()
+    }
+    
+    func loadImage() {
+        let imageURL = "http://placehold.it/1000"
+        URLSession.shared.dataTask(with: URL(string: imageURL)!) { data, response, error in
+            if let imageData = data {
+                let image = UIImage(data: imageData)
+                DispatchQueue.main.async {
+                    self.imageView.image = image
+                }
+            }
+        }.resume()
     }
 }
